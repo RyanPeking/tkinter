4f92070152de7113a8676413e6a2d4f3ab75dbf8
RyanPeking <wushunyan@163.com>
2019-04-13 14:19:38 +0800

    3

--- Tkinter-屏保.py
+++ Tkinter-屏保.py
@@ -1,7 +1,7 @@
 import random
 import tkinter
 
-class RandomBall():
+class RandomBall(object):
     '''
     定义运动的球的类
     '''
@@ -15,6 +15,7 @@ class RandomBall():
         # 球出现的初始位置
         # xpos表示位置的x坐标
 
+        self.canvas = canvas
         self.xpos = random.randint(10, int(scrnwidth)-20)
         # ypos表示位置的y坐标
         self.ypos = random.randint(10, int(scrnheight)-20)
@@ -28,7 +29,7 @@ class RandomBall():
         #定义屏幕的大小
         self.scrnwidth = scrnwidth
         # 定义屏幕的高度
-        self.scrnwidth = scrnheight
+        self.scrnheight = scrnheight
 
         # 球的大小随机
         self.radius = random.randint(20, 120)
@@ -38,9 +39,9 @@ class RandomBall():
         # 在某些系统中，之间用英文单词表示也可以
         # 此处用lambda表达式
         c = lambda: random.randint(0,255)
-        self.color = '#%02x#%02x#%02x' % (c(), c(), c())
+        self.color = '#%02x%02x%02x' % (c(), c(), c())
 
-    def creat_ball(self):
+    def create_ball(self):
         '''
         用构造函数定义的变量值，在canvas上画一个球
         :return:
@@ -57,34 +58,35 @@ class RandomBall():
 
         # fill表示填充颜色
         # outline是外围边框颜色
-        self.item = self.canvas.creat_oval(x1, y1, x2, y2, fill=self.color, outline=self.color)
+        self.item = self.canvas.create_oval(x1, y1, x2, y2, fill=self.color, outline=self.color)
 
     def move_ball(self):
         # 移动球的时候，需要控制球的方向
         # 每次移动后，球都有一个新的坐标
         self.xpos += self.xvelocity
-        self.ypos +=self.yvelocity
+        self.ypos += self.yvelocity
 
         # 以下判断是否撞墙
         # 注意撞墙的算法判断
-        if self.ypos + self.radius >= self.scrnwidth:
+        if self.xpos + self.radius >= self.scrnwidth:
             # 撞到了右边墙
             self.xvelocity = -self.xvelocity
-        if self.xpos + self.radius >= self.scrnheight:
+        if self.ypos + self.radius >= self.scrnheight:
             # 撞到了右边墙
             self.yvelocity = -self.yvelocity
         # 在画布上挪动图画
         self.canvas.move(self.item, self.xvelocity, self.yvelocity)
 
-class ScreenSaver():
+class ScreenSaver(object):
     '''
     定义屏保的类
     可以被启动
     '''
     # 如何装随机产生的球
-    ball = list()
+
 
     def __init__(self):
+        self.balls = list()
         # 每次启动球的数量随机
         self.num_balls = random.randint(6, 20)
 
@@ -98,14 +100,14 @@ class ScreenSaver():
         self.root.bind('<Key>', self.myquit)
 
         # 得到屏幕大小规格
-        w, h = self.root.winfo_sreenwidth(), self.root.winfo_sreenheight()
+        w, h = self.root.winfo_screenwidth(), self.root.winfo_screenheight()
         self.canvas = tkinter.Canvas(self.root, width=w, height=h)
         self.canvas.pack()
 
         # 在画布上画球
         for i in range(self.num_balls):
             ball = RandomBall(self.canvas, scrnwidth=w, scrnheight=h)
-            ball.creat_ball()
+            ball.create_ball()
             self.balls.append(ball)
 
         self.run_screen_saver()
@@ -116,11 +118,11 @@ class ScreenSaver():
             ball.move_ball()
 
         # after是200毫秒后启动一个函数，需要启动的函数是第二个参数
-        self.canvas.after(200, self.run_screen_saver())
+        self.canvas.after(200, self.run_screen_saver)
 
     def myquit(self, e):
         # 此处只是利用了事件处理机制
         # 实际上并不关心事件的类型
         self.root.destory()
 if __name__ == '__main__':
-    ss = ScreenSaver()
+    ScreenSaver()
