6c9009694babba7190cbf08e8ae03e72735456c1
RyanPeking <wushunyan@163.com>
2019-04-16 22:30:27 +0800

    jiasuna

--- 计算器.py
+++ 计算器.py
@@ -18,16 +18,59 @@ show_label = Label(frame_show, textvariable=sv, bg='green', \
                    justify=LEFT, anchor='e')
 show_label.pack(padx=10, pady=10)
 frame_show.pack()
+
+def delete():
+    print("删除")
+
+
+def fan():
+    print("烦了")
+
+
+def clear():
+    print("科利尔")
 # 按键区域
 frame_bord = Frame(width=400, height=350, bg='#cccccc')
+b_del = Button(frame_bord, text="←", width=5,height=1,\
+               command=delete)
+b_del.grid(row=0, column=0)
+button_clear = Button(frame_bord,text = 'C',width = 5,height =1,\
+                      command = clear).grid(row = 0,column = 1)
+button_fan = Button(frame_bord,text = '±',width = 5,height =1,\
+                    command = fan).grid(row = 0,column = 2)
+button_ce = Button(frame_bord,text = 'CE',width = 5,height =1,\
+                   command = clear).grid(row = 0,column = 3)
+num1 = ''
+num2 = ''
+operator = None
 
+def change(num):
+    '''
+    按下一个数字需要考虑
+    :param num:
+    :return:
+    '''
+    # 假如操作数是None，表明肯定是第一个操作数
+    global num1, num2
+    if not operator:
+        num1 += num
+        # 如果是第一个操作数，则只显示第一个操作数
+        sv.set(num1)
+    else:
+        num2 += num
+        sv.set(num1+operator+num2)
+
+
+
+def operation(op):
+    print(op)
+b_1 = Button(frame_bord, text='1', width=5, height=2,\
+             command=lambda:change("1"))
+b_1.grid(row=1, column=0)
+
+b_plus = Button(frame_bord, text='+', width=5, height=2,\
+             command=lambda:operation("+"))
+b_plus.grid(row=2, column=0)
 frame_bord.pack(padx=10, pady=10)
-
-
-
-
-
-
-
 
 root.mainloop()
